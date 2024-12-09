15874767a10a6f7c22c51590996d6986bf44d984
naomity20 <naomity20@gmail.com>
2024-12-09 13:12:09 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -4,8 +4,8 @@ def create_codon_dict(file_path):
     file.close()
     codons_dict = {}
     for row in rows:
-      row_cells = row.strip().split('\t')
-      codons_dict[row_cells[0]] = row_cells[2]
+        row_cells = row.strip().split('\t')
+        codons_dict[row_cells[0]] = row_cells[2]
     return(codons_dict)
 
 
